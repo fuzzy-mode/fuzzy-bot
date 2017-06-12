d5ecde4a6f71bd104cc45e2fba56858340b70efb
Kevin Traver <kevintraver@gmail.com>
2017-06-12 05:33:45 -0600

    use Google Maps Places search instead of Google Geolocation

--- slack-fuzzybot.rb
+++ slack-fuzzybot.rb
@@ -4,6 +4,6 @@ require 'slack-fuzzybot/commands/fuzzytime'
 
 SlackRubyBot.configure do |config|
   Timezone::Lookup.config(:google) { |c| c.api_key = ENV['GOOGLE_MAPS_TIMEZONE_API_KEY'] }
-  Geocoder.configure(lookup: :google, api_key: ENV['GOOGLE_MAPS_API_KEY'], use_https: true)
+  Geocoder.configure(lookup: :google_places_search, api_key: ENV['GOOGLE_MAPS_API_KEY'], use_https: true)
 end
 
