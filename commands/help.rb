f9b946e6d7f6dd072e4c7f1f10e48dd112d0d994
Kevin Traver <kevintraver@gmail.com>
2017-06-22 15:04:16 -0600

    update help command

--- commands/help.rb
+++ commands/help.rb
@@ -1,9 +1,30 @@
-class Help < SlackRubyBot::Commands::Base
-  class << self
-    private
+module SlackRubyBot
+  module Commands
+    class HelpCommand < Base
+      help do
+        title 'help'
+        desc 'Shows help information.'
+      end
 
-    def general_text
-      other_commands_descs = CommandsHelper.instance.other_commands_descs
+      command 'help' do |client, data, match|
+        command = match[:expression]
+
+        text = if command.present?
+                 CommandsHelper.instance.command_full_desc(command)
+               else
+                 general_text
+               end
+
+        client.say(channel: data.channel, text: text, gif: 'help')
+      end
+
+      class << self
+        private
+
+        def general_text
+          other_commands_descs = CommandsHelper.instance.other_commands_descs
+        end
+      end
     end
   end
 end
